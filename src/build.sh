392ad30f36985c74551da34a57c9b61e358f023c
Juan Alvarez <jfhack7@gmail.com>
2023-08-08 03:19:44 -0400

    improvements, typo fixes, and refactoring

--- src/build.sh
+++ src/build.sh
@@ -5,9 +5,9 @@
 # https://stackoverflow.com/a/75629058/2913839
 
 if [ -f "/etc/nv_tegra_release" ]; then
-    echo "Building for Jetson/ARM"
-    DOCKER_BUILDKIT=0 docker build --platform linux/arm64 -t ghcr.io/jfhack/upscaler:latest -f Dockerfile.jetson .
+  echo "Building for Jetson/ARM"
+  DOCKER_BUILDKIT=0 docker build --platform linux/arm64 -t ghcr.io/jfhack/upscaler:latest -f Dockerfile.jetson .
 else
-    echo "Building for x86-64"
-    DOCKER_BUILDKIT=0 docker build --platform linux/amd64 -t ghcr.io/jfhack/upscaler:latest .
+  echo "Building for x86-64"
+  DOCKER_BUILDKIT=0 docker build --platform linux/amd64 -t ghcr.io/jfhack/upscaler:latest .
 fi
